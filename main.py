8dfe87ede97e65c1319f9b8ee903df84fb695306
Michu <michal.dakowicz@infotech.edu.pl>
2024-04-15 17:33:58 +0200

    added threading to speed up the download

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import os
+import threading
 
 os.system('python -m spotdl --download-ffmpeg')
 
 if not os.path.exists('downloads'):
     os.makedirs('downloads')
 
+def download_song(song_link):
+    os.system(f'python -m spotdl {song_link}')
+
 with open('links.txt', 'r') as file:
     os.chdir('downloads')
+    threads = []
     for line in file:
-        os.system(f'python -m spotdl {line}')
+        thread = threading.Thread(target=download_song, args=(line,))
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
